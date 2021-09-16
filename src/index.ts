3b2fe53b974db5da1d3e84dc2dc1220ed829da95
Nuttapong9911 <nuttapongboonsala@gmail.com>
2021-09-16 14:10:23 +0700

    test submiting

--- src/index.ts
+++ src/index.ts
@@ -14,13 +14,45 @@ type Task = {
 
 const tasks: Task[] = []
 
+let currentId: number = 1
+
+app.get('/me', (req, res) => {
+  return res.json({"name" : "Nuttapong Boonsala",
+                  "code" : "630610744"})
+})
+
+app.post('/todo', (req: Request<{}, {}, Task>, res) => {
+if(typeof(req.body.name) === 'string' && typeof(req.body.complete) === 'boolean' && req.body.name !== ""){
+  const newTask: Task ={
+    id : currentId, 
+    name: req.body.name,
+    complete: req.body.complete
+  }
+  // const newTask = req.body
+  tasks.push(newTask)
+  currentId += 1
+
+  return res.json({status : 'success', tasks: tasks})
+
+}else
+  return res.json({status : 'failed', "message" : "Invalid input data"})
+ 
+})
+
+
+
 app.get('/todo', (req, res) => {
+
+  if(req.query.order === "")
+    return res.json({ status: 'success', tasks })
+  // else if (req.query.order === "asc")
+
 
   // try to call /todo?q1=data1&q2data2
   // you can read query parameters with "req.query"
-  console.log(req.query)
+  // console.log(req.query.q1)
  
-  return res.json({ status: 'success', tasks })
+  
 })
 
 
