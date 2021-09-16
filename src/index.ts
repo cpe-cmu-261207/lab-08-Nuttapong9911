a9ff7f47831c996bb55eadd517c7a9f9db5d2100
Nuttapong9911 <nuttapongboonsala@gmail.com>
2021-09-16 15:11:31 +0700

    completed #1

--- src/index.ts
+++ src/index.ts
@@ -14,11 +14,11 @@ type Task = {
 
 const tasks: Task[] = []
 
+
 let currentId: number = 1
 
 app.get('/me', (req, res) => {
-  return res.json({"name" : "Nuttapong Boonsala",
-                  "code" : "630610744"})
+  return res.json({"name" : "Nuttapong Boonsala", "code" : "630610744"})
 })
 
 app.post('/todo', (req: Request<{}, {}, Task>, res) => {
@@ -39,20 +39,76 @@ if(typeof(req.body.name) === 'string' && typeof(req.body.complete) === 'boolean'
  
 })
 
-
-
 app.get('/todo', (req, res) => {
 
-  if(req.query.order === "")
-    return res.json({ status: 'success', tasks })
-  // else if (req.query.order === "asc")
+  if (req.query.order === "asc"){
+    tasks.sort(function(a,b){
+      if(a.name < b.name) { return -1; }
+      if(a.name > b.name) { return 1; }
+      return 0;
+    })
 
+    return res.json({ status: 'success', tasks })
+
+  }else if(req.query.order === "dasc"){
+    tasks.sort(function(a,b){
+      if(a.name < b.name) { return 1; }
+      if(a.name > b.name) { return -1; }
+      return 0;
+    })
+
+    return res.json({ status: 'success', tasks })
+  }else{
+    tasks.sort(function(a,b){
+      if(a.id < b.id) { return -1; }
+      if(a.id > b.id) { return 1; }
+      return 0;
+    })
+    return res.json({ status: 'success', tasks })
+  }
+    
 
   // try to call /todo?q1=data1&q2data2
   // you can read query parameters with "req.query"
   // console.log(req.query.q1)
- 
   
+})
+
+app.put('/todo/mark/:id', (req, res) => {
+    const selectedTask = tasks.find(x => x.id === parseInt(req.params.id))
+    if(selectedTask){
+      selectedTask.complete = !selectedTask.complete
+      tasks.sort(function(a,b){
+        if(a.id < b.id) { return -1; }
+        if(a.id > b.id) { return 1; }
+        return 0;
+      })
+      return res.json({ status: 'success', tasks })
+    }else
+      return res.status(404).json({
+        status: 'failed',
+        message: 'Id not found'
+      })
+
+})
+
+app.delete('/todo/delete/:id', (req, res) => {
+  tasks.sort(function(a,b){
+    if(a.id < b.id) { return -1; }
+    if(a.id > b.id) { return 1; }
+    return 0;
+  })
+  const id = parseInt(req.params.id)
+  const foundIndex = tasks.findIndex(x => x.id === id)
+  if(foundIndex > -1){
+      tasks.splice(foundIndex, 1)
+      return res.json({status: 'success', task: tasks})
+  }else
+      return res.status(404).json({
+          status: 'failed',
+          message: 'Id not found'
+      })
+    
 })
 
 
